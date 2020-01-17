5f3ff035bdd70a7131a9bcc7fcc5fdd6ab005888
imran82ali <code.imranali@gmail.com>
2020-01-17 11:49:12 +0500

    my Weather API Assignment

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+extern crate chrono;
+use chrono::prelude::*;
+
 use async_std::task;
 use surf;
 
@@ -16,12 +19,34 @@ async fn fetch(url: &str) -> Result<String, surf::Exception> {
 
 // execute the fetch function and print the results
 async fn execute() {
-    // my api key 7cbb04e546f1a641693d522fdee48dce
-    match fetch("http://api.openweathermap.org/data/2.5/weather?q=Karachi&units=metric&APPID=7cbb04e546f1a641693d522fdee48dce").await {
+    let key="7cbb04e546f1a641693d522fdee48dce".to_string();
+    let city="Karachi".to_string();
+    let mut url = "http://api.openweathermap.org/data/2.5/weather?q=".to_string();
+    url.push_str(&city);
+    url.push_str("&units=metric&APPID=");
+    url.push_str(&key);
+
+    match fetch(&url).await {
         Ok(s) => {//println!("Fetched results: {:#?}", s);
-            
+        println!("\n We are Feteching Live weather updates from openweathermap.org \n ");
         let data: Vec<&str> = s.split("{").collect();
-         match data.get(7) {
+        //println!("{:#?}",data);
+        match data.get(6) {
+            Some(p)=> {
+            let temp = &p[14..24];
+            let datetime = match temp.parse::<i64>(){
+                Ok(dt) => dt+18000,    
+                Err(_) => 0
+            }; 
+            //println!("{}",datetime+18000);
+            let naive = NaiveDateTime::from_timestamp(datetime, 0);
+            let datetime: DateTime<Utc> = DateTime::from_utc(naive, Utc);
+            let newdate = datetime.format("%Y-%m-%d %H:%M:%S");
+            println!("Date Time : {}", newdate);
+        },//end of some
+            None => println!("Data Not found")
+        };
+        match data.get(7) {
              Some(p)=> {println!("City Name : {}", &p[114..120]);},
              None => println!("Data Not found")
          };
